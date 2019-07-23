34dc508a52cff849473b40ea3445fe4bdee31bb9
Ayman-Barghout <pocean.development@gmail.com>
2019-07-23 11:35:07 +0200

    CRA cleanup and added user search functionality

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ icon, title }) => {
+	return (
+		<nav className='navbar bg-primary'>
+			<h1>
+				<i className={icon} /> {title}
+			</h1>
+		</nav>
+	);
+};
+
+Navbar.propTypes = {
+	icon: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+};
+
+export default Navbar;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+	return (
+		<div className='card text-center'>
+			<img src={avatar_url} alt='' className='round-img' style={{ width: '60px' }} />
+			<h3>{login}</h3>
+			<a href={html_url} className='btn btn-dark btn-sm' target='_blank' rel='noopener noreferrer'>
+				View
+			</a>
+		</div>
+	);
+};
+
+UserItem.propTypes = {
+	login: PropTypes.string,
+	avatar_url: PropTypes.string,
+	html_url: PropTypes.string,
+};
+
+export default UserItem;
